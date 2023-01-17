1b2c31c285fc69610472bc4771aa1505267da014
RebecaTI <mebuffbeka@gmail.com>
2023-01-17 17:18:12 -0300

    voltando para estado do commit alteração

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 console.log("Aqui que foi alterado!")
-console.log("Enviando para o GitHub")
-
