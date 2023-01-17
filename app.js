43b522c01a6b82e4ad2b4f0a9490959434026bfd
RebecaTI <mebuffbeka@gmail.com>
2023-01-17 16:59:35 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 console.log("Aqui que foi alterado!")
+console.log("Enviando para o GitHub")
+
